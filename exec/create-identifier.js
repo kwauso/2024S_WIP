12566301dac0f40066f9c9355265842764f8d73c
kwauso <akiranieda@keio.jp>
2024-08-01 11:33:10 +0900

    update

--- exec/create-identifier.js
+++ exec/create-identifier.js
@@ -1,6 +1,6 @@
 import { agent } from './setup.js';
 export async function createDID(test) {
-    const alias = "www.hi-lite.jp:" + test;
+    const alias = "www.hi-lite.jp#" + test;
     const identifier = await agent.didManagerCreate({ alias: alias, provider: "did:web:www.hi-lite.js" });
     console.log(`New identifier created`);
     console.log(JSON.stringify(identifier, null, 2));
--- src/create-identifier.ts
+++ src/create-identifier.ts
@@ -1,7 +1,7 @@
 import { agent } from './setup.js'
 
 export async function createDID(test: string) {
-  const alias = "www.hi-lite.jp:"+test;
+  const alias = "www.hi-lite.jp#"+test;
   const identifier = await agent.didManagerCreate({ alias: alias, provider: "did:web:www.hi-lite.js"})
   console.log(`New identifier created`)
   console.log(JSON.stringify(identifier, null, 2))
