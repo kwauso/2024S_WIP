488f218ddbe858ea5f9d91d34b70f0dc512c97ec
kwauso <akiranieda@keio.jp>
2024-08-01 12:32:01 +0900

    update

--- exec/delete-identifier.js
+++ exec/delete-identifier.js
@@ -1,14 +1,10 @@
 import { agent } from './setup.js';
-async function main() {
-    const identifier = await agent.didManagerDelete({ did: 'did:web:test' });
-    console.log(`Identifier has deleted.`);
-}
 //This is for delete all DID.
 async function delete_all() {
     const identifiers = await agent.didManagerFind();
     try {
         identifiers.map((id) => {
-            const _identifier = agent.didManagerDelete({ did: `did:web:${id["alias"]}` });
+            agent.didManagerDelete({ did: `${id["did"]}` });
             console.log(`Identifier has deleted.`);
         });
     }
--- src/create-identifier.ts
+++ src/create-identifier.ts
@@ -1,11 +1,10 @@
 import { agent } from './setup.js'
 
 export async function createDID(test: string) {
-  const alias = "did:web:www.hi-lite.jp:"+test;
+  const alias = "www.hi-lite.jp"+test;
   const identifier = await agent.didManagerCreate({ alias: alias, provider: "did:web:www.hi-lite.js"})
   console.log(`New identifier created`)
   console.log(JSON.stringify(identifier, null, 2))
 }
 
-//main().catch(console.log)
-//comment
+//main().catch(console.log):
